f12e7233aa7159d02dabd8e1333db8bd426df6ec
lucas.granela <lucas.granela@wr.sc.usp.br>
2020-10-23 14:16:06 -0300

    Introducao do que deve ser produzido

--- Trabalho1.c
+++ Trabalho1.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 
 int main (){
-    
+    /*
+    * O programa possui 5 tipos de entrada: 
+    *   caso 1: ler arquivos em formato csv e gravar em arquivos de dados de saida 
+    *   
+    *   caso 2: recuperar os dados do arquivo pessoa e mostrar de forma organizada
+    * 
+    *   caso 3: busca por determinado usuario
+    *   
+    *   caso 4: insercao de registro adicionais
+    * 
+    *   caso 5: atualizacao dos campos 
+    */
+
     return 0;
 }
