cff035b899b1d378f0dea256bd0d6b59e5bf7eb0
lucas.granela <lucas.granela@wr.sc.usp.br>
2020-10-26 12:09:34 -0300

    TAD FOSORIO

--- arqIndicePessoa.c
+++ arqIndicePessoa.c
@@ -2,3 +2,160 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+Lista* cria_lista() {
+    Lista* li = (Lista*) malloc(sizeof(Lista));
+
+    if (li != NULL)
+        *li = NULL;
+
+    return li;
+}
+
+
+
+void libera_lista(Lista* li) {
+    if (li != NULL) {
+        Elem* no;
+
+        while ((*li) != NULL) {
+            no = *li;
+            *li = (*li)->prox;
+
+            free(no);
+        }
+
+        free(li);
+    }
+}
+
+
+
+int consulta_lista_pos(Lista* li, int pos, Tipo_Dado *dt) {
+    if (li == NULL || pos <= 0)
+        return ERRO;
+    
+    Elem *no = *li;
+
+    int i = 1;
+
+    while (no != NULL && i < pos){
+        no = no->prox;
+        i++;
+    }
+
+    if (no == NULL)
+        return ERRO;
+    else {
+        *dt = no->dado;
+        return OK;
+    }
+}
+
+int consulta_lista_dado(Lista* li, Tipo_Dado dt, Elem **el) {
+    if (li == NULL)
+        return 0;
+
+    Elem *no = *li;
+
+    while (no != NULL && no->dado != dt){
+        no = no->prox;
+    }
+
+    if (no == NULL)
+        return ERRO;
+    else {
+        *el = no;
+        return OK;
+    }
+}
+
+int insere_lista_final(Lista* li, Tipo_Dado dt) {
+    Elem *no;
+    
+    if (li == NULL) return ERRO;
+
+    no = (Elem*) malloc(sizeof(Elem));
+
+    if (no == NULL)  return ERRO;
+
+    no->dado = dt;
+    no->prox = NULL;
+
+	if ((*li) == NULL) {   //lista vazia: insere in�cio
+        no->ant = NULL;
+        *li = no;
+    } else {
+        Elem *aux;
+        aux = *li;
+        
+        while (aux->prox != NULL){
+            aux = aux->prox;
+        }
+
+        aux->prox = no;
+        no->ant = aux;
+    }
+
+    return OK;
+}
+
+
+int remove_lista(Lista* li, Tipo_Dado dt) {   //TERMINAR
+    if (li == NULL)
+        return ERRO;
+
+    if ((*li) == NULL)//lista vazia
+        return ERRO;
+
+    Elem *no = *li;
+
+    while (no != NULL && no->dado != dt){
+        no = no->prox;
+    }
+
+    if (no == NULL)//n�o encontrado
+        return ERRO;
+
+    if (no->ant == NULL)//remover o primeiro?
+        *li = no->prox;
+    else
+        no->ant->prox = no->prox;
+
+    if (no->prox != NULL)//n�o � o �ltimo?
+        no->prox->ant = no->ant;
+
+    free(no);
+    
+    return OK;
+
+}
+
+int tamanho_lista(Lista* li) {
+    if (li == NULL)
+        return 0;
+
+    int cont = 0;
+
+    Elem* no = *li;
+
+    while (no != NULL){
+        cont++;
+        no = no->prox;
+    }
+
+    return cont;
+}
+
+void imprime_lista(Lista* li) {
+    Elem* no = *li;
+
+    if (li == NULL)
+        return;
+
+    while (no != NULL) {
+        printf("Dado: %5d # Ant: %p - Dado: %p - Prox: %p\n",no->dado,no->ant,no,no->prox);
+        no = no->prox;
+    }
+    printf("-------------- FIM LISTA -----------------\n");
+}
--- arqIndicePessoa.h
+++ arqIndicePessoa.h
@@ -1,6 +1,13 @@
 #ifndef ARQINDICEPESSOA_H
 #define ARQINDICEPESSOA_H
 
+#define FALSO      0
+#define VERDADEIRO 1
+
+#define OK         1
+#define ERRO       0
+
+
 typedef int Tipo_Dado;
 
 struct elemento{
@@ -12,5 +19,13 @@ struct elemento{
 typedef struct elemento Elem;
 typedef struct elemento* Lista;
 
+Lista* cria_lista();
+void libera_lista(Lista* li);
+int consulta_lista_pos(Lista* li, int pos, Tipo_Dado *dt);
+int consulta_lista_dado(Lista* li, Tipo_Dado dt, Elem **el);
+int insere_lista_final(Lista* li, Tipo_Dado dt);
+int remove_lista(Lista* li, Tipo_Dado dt);
+int tamanho_lista(Lista* li);
+void imprime_lista(Lista* li);
 
 #endif
